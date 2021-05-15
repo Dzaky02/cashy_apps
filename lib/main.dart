2469aa380bda76a2ea0c839ceb1b1f4f5f5d197a
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-05-15 08:08:23 +0700

    Nested Widget & Icon Button

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -17,6 +18,9 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Cashy"),
           backgroundColor: Colors.red,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.mail), onPressed: () {})
+          ],
         ),
         body: SafeArea(child: Text("Flutter Pertama",)),
       ),
